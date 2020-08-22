57de1b5f7481696234ce3d8a084ce4fe9cc35ee5
User <User@DESKTOP-VFTMQ9S>
2020-08-23 00:53:55 +0900

    2020/08/23 00:53

--- src/User/User.java
+++ src/User/User.java
@@ -1,5 +1,44 @@
 package User;
 
 public class User {
-
+	String userID;
+	String userPassoword;
+	String userName;
+	String userGender;
+	String userEmail;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	
+	public String getUserPassoword() {
+		return userPassoword;
+	}
+	public void setUserPassoword(String userPassoword) {
+		this.userPassoword = userPassoword;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
 }
